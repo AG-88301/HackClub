29c030e61517b7446d28f726b5ebf36b9daf5ca2
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-06-18 19:51:47 +0100

    parse input

--- evaluateExpression.py
+++ evaluateExpression.py
@@ -1,1 +1,44 @@
 
+def calc(expression):
+    expression = expression.replace(" ", "").replace("--", '+').replace('+-', '-').replace('-+', '-')
+    expr = ['']
+    ind = 0
+    while ind < len(expression):
+        i = expression[ind]
+        if i == '-':
+            if expr[-1] == '-':
+                expr[-1] = '+'
+            elif expr[-1] == '+':
+                expr[-1] = '-'  
+            elif expr[-1] == '':
+                expr[-1] = '-o'
+                expr.append('')
+            else:
+                expr.append('-')
+                expr.append('')
+                
+        elif i in '+*/':
+            expr.append(i)
+            expr.append('')
+            
+        elif i == '(':
+            depth = 1
+            brack = ""
+            ind += 1
+            while depth:
+                if expression[ind] == '(':
+                    depth += 1
+                elif expression[ind] == ')':
+                    depth -= 1
+                brack += expression[ind]
+                ind += 1
+            ind -= 1
+            expr[-1] = calc(brack[:-1])
+        else:
+            if expr[-1] == '': expr[-1] = 0
+            expr[-1] = expr[-1] * 10 + float(i)
+        ind += 1
+        
+    return expr
+expression = input()
+print(calc(expression))
