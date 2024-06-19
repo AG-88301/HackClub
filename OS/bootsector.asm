1158f7a4b7e0b86c894769819a9aa68b2d8066df
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-06-19 15:07:29 +0100

    see previous commit

--- OS/bootsector.asm
+++ OS/bootsector.asm
@@ -1,18 +1,35 @@
 [org 0x7c00]
+mov bp, 0x8000
+mov sp, bp
 
-mov bx, HELLO
+mov bx, 0x9000
+mov dh, 2
+call disk_load
+
+; test
+mov bx, MESSAGE
 call print
+
 call print_nl
 
-mov dx, 0x23ff
+mov dx, [0x9000]
+call print_hex
+
+call print_nl
+
+mov dx, [0x9000 + 512]
 call print_hex
 
 jmp $
 
 %include "bootsector_print.asm"
+%include "bootsector_disk.asm"
 
-; vars
-HELLO: db 'Hello World!', 0
+MESSAGE: db 'Retrieved from disk:', 0
 
-times 510-($-$$) db 0
+times 510 - ($-$$) db 0
 dw 0xaa55
+
+; test data
+times 256 dw 0xde43
+times 256 dw 0xff1f
