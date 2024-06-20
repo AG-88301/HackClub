12726c3d8963cd2c6cbadc8cdd5f5a06c9d7d175
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-06-20 17:29:17 +0100

    added functionality to graph

--- app/main/graphing.py
+++ app/main/graphing.py
@@ -1,17 +1,40 @@
-from dash import dcc, html, Input, Output
+from dash import dcc, html, Input, Output, State
 from django_plotly_dash import DjangoDash
 import plotly.graph_objects as go
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 
 app = DjangoDash('Graph')
-app.layout = html.Div([dcc.Graph(id='graph'), dcc.Input(id="inp")], style={'height':'70vh'})
+app.layout = html.Div([
+    dcc.Store(id="memory", data={'n':0, 'lines':[]}), 
+    html.Div([
+        html.Div([
+            dcc.Graph(id='graph'), 
+        ], style={'width':'45vw'}),
+        html.Div([
+            html.Label('X:')
+        ], style={'width':'45vw', 'padding':'2% 2% 2% 2%'}),
+    ], style={'display':'flex', 'flex-direction':'row'}),
+    html.Button(id='newLineButton', children='+')
+])
 
 @app.callback(
     Output('graph', 'figure'),
-    Input('inp', 'value')
+    Input('memory', 'data')
 )
 def update_figure(inp):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=[1,2,3], y=[5,3,6], mode='lines'))
-    fig.add_trace(go.Scatter(x=[14,23,45], y=[3,4,9], mode='lines'))
+    for line in inp['lines']:
+        fig.add_trace(line)
     return fig
+
+@app.callback(
+    Output('memory', 'data'),
+    Input('newLineButton', 'n_clicks'),
+    State('memory', 'data')
+)
+def upd(n_clicks, mem):
+    if n_clicks is None: raise PreventUpdate
+    mem['n'] += 1
+    mem['lines'].append(go.Scatter(x=[mem['n']*1,2,3], y=[5,3,6], mode='lines'))
+    return mem
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -7,6 +6,6 @@ from . import graphing
 def index(request):
     template = loader.get_template('index.html')
     context = {
-        'fruits': ['apple', 'banana', 'cherry'],
+        'n':0,
     }
     return HttpResponse(template.render(context, request))
