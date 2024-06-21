77d045d52cbc93590a5bf23e9ec2e86950869451
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-06-21 12:50:12 +0100

    learning basics of inline machine code

--- OS/kernel/kernel.c
+++ OS/kernel/kernel.c
@@ -1,6 +1,16 @@
+#include "../drivers/driver_videoport.c"
+
 void main() {
-    char* video = (char*) 0xb8000;
-    *video = 'X';
-    *(video + 2) = 'Y';
-    *(video + 4) = 'Z';
+    port_byte_out(0x3d4, 14);
+    int position = port_byte_in(0x3d5);
+    position = position << 8;
+
+    port_byte_out(0x3d4, 15);
+    position += port_byte_in(0x3d5);
+
+    int offset_from_vga = position * 2;
+
+    char *vga = 0xb8000;
+    vga[offset_from_vga] = 'X'; 
+    vga[offset_from_vga+1] = 0x0f;
 }
